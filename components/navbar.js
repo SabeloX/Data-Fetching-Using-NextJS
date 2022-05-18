5c5c61fada8d1cbb80f7fbd84aae2f1add6a78cf
Sabelo <sabelomtw@gmail.com>
2022-05-18 21:44:10 +0200

    Added a logo using next Image

--- components/navbar.js
+++ components/navbar.js
@@ -1,10 +1,15 @@
+import Image from "next/image"
 import Link from "next/link"
 
 export const Navbar = () => {
     return (
         <nav>
             <div className="logo">
-                <h1>People</h1>
+                <Image
+                    src="/logo.png"
+                    width={128}
+                    height={128}
+                />
             </div>
             <Link href="/">Home</Link>
             <Link href="/about">About</Link>
