615e6f1cda924fe22b6e1df14feb3a43f86c8dc6
Sabelo <sabelomtw@gmail.com>
2022-05-18 21:50:36 +0200

    Added Head data

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,23 @@
+import Head from 'next/head'
 import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div>
-      <h1 className={styles.title}>Homepage</h1>
-      <p className={styles.text}>Amet minim nisi est consequat ad amet enim. Officia excepteur ex labore occaecat tempor culpa est cupidatat. Esse labore exercitation exercitation adipisicing quis magna ex ea incididunt nostrud deserunt ea nisi.</p>
-      <Link href="/people">
-        <a className={styles.button}>
-          See People List
-        </a>
-      </Link>
-    </div>
+    <>
+      <Head>
+        <title>People List | Home</title>
+        <meta name="keywords" content="people"/>
+      </Head>
+      <div>
+        <h1 className={styles.title}>Homepage</h1>
+        <p className={styles.text}>Amet minim nisi est consequat ad amet enim. Officia excepteur ex labore occaecat tempor culpa est cupidatat. Esse labore exercitation exercitation adipisicing quis magna ex ea incididunt nostrud deserunt ea nisi.</p>
+        <Link href="/people">
+          <a className={styles.button}>
+            See People List
+          </a>
+        </Link>
+      </div>
+    </>
   )
 }
