6a23cd081555586fd4d3ae17f967dd15d1e0ba41
Sabelo <sabelomtw@gmail.com>
2022-05-18 21:36:21 +0200

    Used useRouter to redirect

--- pages/404.js
+++ pages/404.js
@@ -1,6 +1,15 @@
 import Link from "next/link"
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default () => {
+    const router = useRouter();
+
+    useEffect(() => {
+        setTimeout(() => {
+            router.push("/");
+        }, 3000);
+    } ,[]);
     return (
         <div className="not-found">
             <h1>Ooopse...</h1>
