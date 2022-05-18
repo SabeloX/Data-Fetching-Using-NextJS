21a1cce244fc4d374094f1bca1362f6801ee3b25
Sabelo <sabelomtw@gmail.com>
2022-05-18 21:26:39 +0200

    Applied home styles

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,16 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import Link from 'next/link'
-import { Footer } from '../components/footer'
-import { Navbar } from '../components/navbar'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div>
-      <h1>Homepage</h1>
-      <p>Amet minim nisi est consequat ad amet enim. Officia excepteur ex labore occaecat tempor culpa est cupidatat. Esse labore exercitation exercitation adipisicing quis magna ex ea incididunt nostrud deserunt ea nisi.</p>
-      <Link href="/people">See People List</Link>
+      <h1 className={styles.title}>Homepage</h1>
+      <p className={styles.text}>Amet minim nisi est consequat ad amet enim. Officia excepteur ex labore occaecat tempor culpa est cupidatat. Esse labore exercitation exercitation adipisicing quis magna ex ea incididunt nostrud deserunt ea nisi.</p>
+      <Link href="/people">
+        <a className={styles.button}>
+          See People List
+        </a>
+      </Link>
     </div>
   )
 }
