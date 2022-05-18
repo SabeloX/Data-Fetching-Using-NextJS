7bee7d95f1011e9529225438e1b3748719e5fedb
Sabelo <sabelomtw@gmail.com>
2022-05-18 21:16:26 +0200

    Added global styles

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
 import { Footer } from '../components/footer'
 import { Navbar } from '../components/navbar'
 import styles from '../styles/Home.module.css'
@@ -8,6 +9,8 @@ export default function Home() {
   return (
     <div>
       <h1>Homepage</h1>
+      <p>Amet minim nisi est consequat ad amet enim. Officia excepteur ex labore occaecat tempor culpa est cupidatat. Esse labore exercitation exercitation adipisicing quis magna ex ea incididunt nostrud deserunt ea nisi.</p>
+      <Link href="/people">See People List</Link>
     </div>
   )
 }
