e0ab04593780cf349ba7a675d5ebe4f272d1cf6d
Sabelo <sabelomtw@gmail.com>
2022-05-18 22:02:06 +0200

    Added styles to people list

--- pages/people/index.js
+++ pages/people/index.js
@@ -1,10 +1,26 @@
-import { Footer } from "../../components/footer"
-import { Navbar } from "../../components/navbar"
+import styles from "../../styles/People.module.css";
 
-export default () => {
+export const getStaticProps = async () => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/users");
+    const data = await res.json();
+    return {
+        props: { people: data }
+    }
+}
+
+export default ({ people }) => {
     return(
         <div>
             <h1>This is the root for people</h1>
+            {
+                people.map(person => (
+                    <a className={styles.single}>
+                        <div key={person.id}>
+                            <h3>{person.name}</h3>
+                        </div>
+                    </a>
+                ))
+            }
         </div>
     )
 }
